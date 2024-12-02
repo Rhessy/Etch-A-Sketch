248ac3969f31d7307b8cb5f2edbb643661c32324
Riley Hess <RileyH@beckhoff.com>
2024-12-02 11:40:13 -0600

    adding in read me to describe the functionality of the web app

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Etch-A-Sketch
 Using Javascript to manipulate the DOM in order to create an Etch-A-Sketch game that is accessible via the browser.
+
+------ Description ------
+
+1. On load the Etch-A-Sketch web app will load a 16x16 grid of divs
+2. these divs will have event listeners on them which will fill the background on hover
+3. there will be input for the user to edit the grid to their liking, 2x2, 4x4, ..., 100x100
+
+
+------ TBD ------
+
+1. adding in Rng for random color when hovering over div 'pixel'
+2. when hovering over a div 'pixel' that has color, will darken the color until it reaches black
--- app.js
+++ app.js
@@ -1,15 +1,60 @@
 let container = document.querySelector('.container');
 let pixels = [];
+let x = document.getElementById('x');
+let y = document.getElementById('y');
+let x_value = x.value;
+let y_value = y.value;
+x.addEventListener('click', ()=>{
+    x_value = x.value;
+    console.log(x_value);
+    let dim = x_value*y_value;
+    createEtchASketch();
+})
+
+y.addEventListener('click', ()=>{
+    y_value = y.value;
+    console.log(y_value);
+    let dim = x_value*y_value;
+    createEtchASketch();
+
+})
 
 
-for(i=0; i<16; i++){
-    pixels[i] = document.createElement('div');
-    pixels[i].className = 'pixel';
+let dim = x_value*y_value;
+
+
+
+
+
+
+
+console.log(dim);
+
+
+function createEtchASketch(){
+    let dim = x_value*y_value;
+
+    console.log('I made it to the function and this is the value of dim:' + dim);
+
+    for(i=0; i<dim; i++){
+        pixels[i] = document.createElement('div');
+        pixels[i].className = 'pixel';
+        pixels[i].style.height = 800/y_value +'px';
+        pixels[i].style.width = 800/x_value +'px';
+        pixels[i].style.backgroundcolor = 'red';
+    }
+    
+    
+    
+    
+    for(i=0; i< dim; i++){
+        container.appendChild(pixels[i]);
+        console.log('I made it');
+    }
 }
 
-for(i=0; i< 16; i++){
-    container.appendChild(pixels[i]);
-    console.log('I made it');
-}
+
+
+
 
 
